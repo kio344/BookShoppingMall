f2d33d2b77239e0c426fd47a813f5a9a41d373a0
MinHoKim <kimm3654@naver.com>
2022-10-17 19:02:05 +0900

    admin/board/AdminBoardController 추가, view/admin/board/adminBoard.html추가, view/outline/nav/admin.html 수정

---
+++ src/main/java/controllers/admin/board/AdminBoardController.java
@@ -0,0 +1,16 @@
+package controllers.admin.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/board")
+public class AdminBoardController {
+
+	@GetMapping
+	public String adminBoard() {
+		
+		return "admin/board/adminBoard";
+	}
+}
