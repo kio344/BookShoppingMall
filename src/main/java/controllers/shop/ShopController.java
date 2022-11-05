d6ae7d32a8bc29cfb5f8ea603cb111c0b6fc5ecf
bmSrv <jmspon33@gmail.com>
2022-11-05 21:03:35 +0900

    쇼핑몰 메인페이지 구성중

--- src/main/java/controllers/shop/ShopController.java
+++ src/main/java/controllers/shop/ShopController.java
@@ -1,20 +1,49 @@
 package controllers.shop;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import models.shop.ProductDto;
+import models.shop.service.ShopService;
 
 @Controller
 @RequestMapping("/shop")
 public class ShopController {
 	
+	@Autowired
+	private ShopService service;
+	
 	@GetMapping("/index")
 	public String shop(Model model) {
-		String[] addJs=new String[] {"shop/index"};
+		String[] addJs=new String[] {"/shop/index"};
+		String[] addCss=new String[] {"/shop/index"};
 		model.addAttribute("addJs",addJs);
+		model.addAttribute("addCss",addCss);
 		
+		List<ProductDto> newProductList=service.getProducts(0,5);
+		model.addAttribute("newProductList",newProductList);
+
+
+		List<ProductDto> bestSellerList=service.getBestSeller(0, 5);
+		model.addAttribute("bestSellerList",bestSellerList);
 		return "shop/shop";
+	}
+	
+	@ResponseBody
+	@GetMapping("/product/{productnum}")
+	public ProductDto product(@PathVariable(name = "productnum" )long productnum) {
+
+
+		return service.getProduct(productnum);
 	}
 	
 
