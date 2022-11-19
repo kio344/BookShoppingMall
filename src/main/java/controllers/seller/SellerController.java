62b0f73584fea5ca85ec028f6a9696f67bb57e07
Baeminseo <qoalstj01@naver.com>
2022-11-20 01:21:29 +0900

    seller 메인 페이지 완성, seller 세부페이지 css 추가중

--- src/main/java/controllers/seller/SellerController.java
+++ src/main/java/controllers/seller/SellerController.java
@@ -1,13 +1,16 @@
 package controllers.seller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SellerController {
 	
 	@RequestMapping("/seller")
-	public String seller() {
+	public String seller(Model model) {
+		
+		model.addAttribute("addCss", new String [] {"/seller/sellerMain"});
 		
 		return "seller/seller";
 	}
