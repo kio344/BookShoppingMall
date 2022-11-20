d50c05a2a1e8ec205b37b6ae6dcab87250a56e3d
bmSrv <jmspon33@gmail.com>
2022-11-20 19:48:18 +0900

    PaymentController 주석 추가

--- src/main/java/controllers/shop/PaymentController.java
+++ src/main/java/controllers/shop/PaymentController.java
@@ -33,10 +33,10 @@ public class PaymentController {
 
 	@Autowired
 	private TossService tossService;
-	
-	@Autowired 
+
+	@Autowired
 	private PaymentValidation validation;
-	
+
 	@Autowired
 	private LoginService loginService;
 
@@ -49,6 +49,13 @@ public class PaymentController {
 	@Autowired
 	private ShopController shopController;
 
+	/**
+	 * 상품 상세 페이지에서
+	 * 결제 페이지로 이동 
+	 * @param productNum
+	 * @param model
+	 * @return
+	 */
 	public String goPayment(Long productNum, Model model) {
 
 		PaymentRequest paymentRequest = paymentService.paymentSetting(productNum);
@@ -64,6 +71,13 @@ public class PaymentController {
 		return "shop/payment";
 	}
 
+	/**
+	 * 결제 필수 데이터 검증실패 시
+	 * 결제페이지로 이동
+	 * @param request
+	 * @param model
+	 * @return
+	 */
 	public String goPayment(PaymentRequest request, Model model) {
 
 		ProductDto product = shopService.getProduct(request.getProductNum());
@@ -97,6 +111,13 @@ public class PaymentController {
 
 	}
 
+	/**
+	 * 결제 검증 실패시 error 표출
+	 * @param paymentRequest
+	 * @param error
+	 * @param model
+	 * @return
+	 */
 	@PostMapping("/payment/processErr")
 	public String payment(@Valid PaymentRequest paymentRequest, Errors error, Model model) {
 		PaymentRequest request = paymentRequest;
@@ -115,13 +136,13 @@ public class PaymentController {
 		result.setResult(true);
 
 		try {
-			
+
 			validation.validate(paymentRequest, error);
 
 			result.setData(paymentRequest);
-			
+
 		} catch (RuntimeException e) {
-			
+
 			result.setResult(false);
 			result.setMessage(e.getMessage());
 
@@ -130,6 +151,7 @@ public class PaymentController {
 		return ResponseEntity.ok(result);
 	}
 
+	
 	@ResponseBody
 	@PostMapping("/payment/process")
 	public PaymentDto paymentProcess(PaymentRequest paymentRequest) {
@@ -140,20 +162,40 @@ public class PaymentController {
 
 	}
 
+	/**
+	 * 토스 결제 성공 시
+	 * payment:prgoress 업데이트
+	 * tossCallBack 처리 
+	 * 
+	 * @param orderId
+	 * @param paymentKey
+	 * @param amount
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	@GetMapping(produces = "text/html;charset=utf-8", path = "/payment/result/sc")
 	public String processSc(String orderId, String paymentKey, Long amount, Model model, HttpSession session) {
 		String paymentId = orderId.split("__")[1];
-		
-		paymentService.updateProgress(Long.parseLong(paymentId), PaymentProgress.PAYMENT_COMPLET,orderId);
 
+		paymentService.updateProgress(Long.parseLong(paymentId), PaymentProgress.PAYMENT_COMPLET, orderId);
 
 		tossService.paymentCallBackSc(orderId, paymentKey, amount);
-		
-
 
 		return shopController.shop(model, session);
 	}
 
+	/**
+	 * 토스 결제 실패 시
+	 * payment 삭제
+	 * tossCallBack 처리 
+	 * @param orderId
+	 * @param paymentKey
+	 * @param amount
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	@GetMapping(produces = "text/html;charset=utf-8", path = "/payment/result/fail")
 	public String processFail(String orderId, String paymentKey, Long amount, Model model, HttpSession session) {
 		String paymentId = orderId.split("__")[1];
