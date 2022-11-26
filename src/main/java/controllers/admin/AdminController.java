058db94038c31f8fbbd3c2e3a654294e030fb279
Baeminseo <qoalstj01@naver.com>
2022-11-26 23:11:07 +0900

    admin 페이지 css 추가중

--- src/main/java/controllers/admin/AdminController.java
+++ src/main/java/controllers/admin/AdminController.java
@@ -1,13 +1,16 @@
 package controllers.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
 	
 	@RequestMapping("/admin")
-	public String adminPage() {
+	public String adminPage(Model model) {
+		
+		model.addAttribute("addCss", new String[] { "/board/admin/admin" });
 		
 		return "admin/admin";
 		
